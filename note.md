17e49af159b72b62d7e6fea066ec7e9863af1c06
sanfeng8848 <beyondme121@163.com>
2021-02-19 16:52:34 +0800

    1. 配置基本的跨域,安全策略,访问限制,请求端口与api前缀等信息

---
+++ note.md
@@ -0,0 +1,2 @@
+### helmet
+> helmet是express的中间件，通过设置各种header来为express应用提供安全保护。虽然不能完全杜绝安全问题，但确实能提供某种程度的保护
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+// 中间件
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+
+const HOST = process.env.HOST || 'locahost';
+const PORT = process.env.PORT || 8080;
+const PREFIX = process.env.PREFIX || '/';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // 1. 跨域
+  app.enableCors();
+  // 2. 通过设置请求提供安全保护
+  app.use(helmet());
+  // 3. 访问限制
+  app.use(
+    rateLimit({
+      windowMS: 15 * 60 * 1000,
+      max: 3, // limit each IP to 100 requests per windowMs
+    }),
+  );
+  // 配置请求api前缀
+  app.setGlobalPrefix(PREFIX);
+  await app.listen(PORT, HOST, () => {});
+  Logger.log(`server start at ${HOST}:${PORT}/${PREFIX}`);
 }
 bootstrap();
